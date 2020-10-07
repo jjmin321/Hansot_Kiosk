94c4939096f4039dc457e2fc9dfa2f575273ad02
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-10-07 11:55:10 +0900

    [UPDATE] UserControlOrder layout update & 카테고리 기능 add
    UserControlOrder의 layout을 listbox로 바꾸고 UserControlOrder에 카테고리 기능을 추가했다

---
+++ Hansot_Kiosk/View/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hansot_Kiosk.View
+{
+        public enum Category
+        {
+            SPECIAL,
+            PRIMIUM,
+            WAPPER,
+        }
+}
--- Hansot_Kiosk/View/UserControlOrder.xaml.cs
+++ Hansot_Kiosk/View/UserControlOrder.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hansot_Kiosk.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,40 @@ namespace Hansot_Kiosk
     /// </summary>
     public partial class UserControlOrder : UserControl
     {
+        private List<Food> lstFood = new List<Food>()
+        {
+            new Food(){ category = Category.SPECIAL, name = "ㅇㅇ도시락", imagePath = @"/Assets/기네스머쉬룸와퍼.png" },
+            new Food(){ category = Category.SPECIAL, name = "xx도시락", imagePath = @"/Assets/기네스와퍼.png" },
+            new Food(){ category = Category.SPECIAL, name = "asdf도시락", imagePath = @"/Assets/기네스콰트로치즈와퍼.png" },
+
+            new Food(){ category = Category.PRIMIUM, name = "김치덮밥", imagePath = @"/Assets/기네스와퍼팩1.png" },
+            new Food(){ category = Category.PRIMIUM, name = "불고기덮밥", imagePath = @"/Assets/기네스와퍼팩2.png" },
+            new Food(){ category = Category.PRIMIUM, name = "ㅁㄴㅇ덮밥", imagePath = @"/Assets/기네스와퍼팩3.png" },
+
+            new Food(){ category = Category.WAPPER, name = "콜라", imagePath = @"/Assets/와퍼.png" },
+            new Food(){ category = Category.WAPPER, name = "사이다", imagePath = @"/Assets/불고기와퍼.png" },
+            new Food(){ category = Category.WAPPER, name = "ㅁㄴㅇㄹ", imagePath = @"/Assets/치즈와퍼.png" },
+        };
+
         public UserControlOrder()
         {
             InitializeComponent();
+
+            this.FontFamily = new System.Windows.Media.FontFamily("배달의민족 도현");
+
+            this.Loaded += MainWindow_Loaded;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            lbCategory.SelectedIndex = 0; //처음 실행 시 첫번째 카테고리가 선택되도록 
+        }
+
+        private void lbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbCategory.SelectedIndex == -1) return;
+
+            Category category = (Category)lbCategory.SelectedIndex;
+            lbMenus.ItemsSource = lstFood.Where(x => x.category == category).ToList();
         }
         private void MoveToHome(object sender, RoutedEventArgs e)
         {
