ac5d08635b14ca0c8b54948641e139d86b8f97e3
jjmin321 <jjmin321@naver.com>
2020-11-01 02:33:28 +0900

    [ADD] 모델 추가

---
+++ Hansot_Kiosk/Model/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hansot_Kiosk.Model
+{
+    public class Menu
+    {
+        /// <summary>
+        /// 카테고리명
+        /// </summary>
+        public String Category { get; set; }
+
+        /// <summary>
+        /// 음식 이름
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// 음식 가격
+        /// </summary>
+        public int Price { get; set; }
+
+        /// <summary>
+        /// 음식 이미지 경로
+        /// </summary>
+        public String ImagePath { get; set; }
+    }
+}
---
+++ Hansot_Kiosk/Model/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hansot_Kiosk.Model
+{
+    public class Payment
+    {
+        /// <summary>
+        /// 유저 이름
+        /// </summary>
+        public String UserName { get; set; }
+
+        /// <summary>
+        /// 메뉴 이름
+        /// </summary>
+        public String MenuName { get; set; }
+
+        /// <summary>
+        /// 음식 주문 갯수
+        /// </summary>
+        public int MenuCount { get; set; }
+    }
+}
