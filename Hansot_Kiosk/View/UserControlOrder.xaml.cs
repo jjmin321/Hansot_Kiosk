22f27a75ad17f43bafe3310151054a7b08e0489a
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-11-01 15:16:34 +0900

    [UPDATE] change total price calculation & count
    총합 계산하는 방법이랑 카운트 올리는 방식을 바꿈

--- Hansot_Kiosk/View/UserControlOrder.xaml.cs
+++ Hansot_Kiosk/View/UserControlOrder.xaml.cs
@@ -15,7 +15,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using UIStateManagerLibrary;
-
 namespace Hansot_Kiosk.View
 {
     /// <summary>
@@ -23,73 +22,186 @@ namespace Hansot_Kiosk.View
     /// </summary>
     public partial class UserControlOrder : CustomControlModel
     {
+        private int totalPrice = 0;
+
         private List<Food> calcaulation = new List<Food>()
         {
-                        
+            //담겨지는 리스트
         };
 
         private List<Food> lstFood = new List<Food>()
         {
-            new Food(){ category = Category.lunchBox, name = "불고기도시락", count=1 ,price=3000, imagePath = @"/Static/불고기도시락.jpg" },
-            new Food(){ category = Category.lunchBox, name = "새우튀김도시락", price=4000, count=1, imagePath = @"/Static/새우튀김도시락.png" },
-            new Food(){ category = Category.lunchBox, name = "세우튀김스테이크도시락", price=5000,count=1, imagePath = @"/Static/새우튀김스테이크도시락.png" },
-            
-            new Food(){ category = Category.RiceBowl, name = "참치마요덮밥", price=3500, count=1,imagePath = @"/Static/참치마요덮밥.jpg" },
-            new Food(){ category = Category.RiceBowl, name = "참치비빔덮밥",count=1, price=3200,imagePath = @"/Static/참치비빔덮밥.jpg" },
-            new Food(){ category = Category.RiceBowl, name = "치킨마요덮밥",count=1, price=3700, imagePath = @"/Static/치킨마요덮밥.jpg" },
+            new Food(){ category = Category.lunchBox, name = "불고기도시락", count=0 ,price=3000, imagePath = @"/Static/불고기도시락.jpg" },
+            new Food(){ category = Category.lunchBox, name = "새우튀김도시락", price=4000, count=0, imagePath = @"/Static/새우튀김도시락.png" },
+            new Food(){ category = Category.lunchBox, name = "세우튀김스테이크도시락", price=5000,count=0, imagePath = @"/Static/새우튀김스테이크도시락.png" },
 
-            new Food(){ category = Category.juice, name = "콜라", price=1500, count=1,imagePath = @"/Static/콜라.jpg" },
-            new Food(){ category = Category.juice, name = "사이다",price=1500, count=1,imagePath = @"/Static/칠성사이다.jpg" },
+            new Food(){ category = Category.RiceBowl, name = "참치마요덮밥", price=3500, count=0,imagePath = @"/Static/참치마요덮밥.jpg" },
+            new Food(){ category = Category.RiceBowl, name = "참치비빔덮밥",count=0, price=3200,imagePath = @"/Static/참치비빔덮밥.jpg" },
+            new Food(){ category = Category.RiceBowl, name = "치킨마요덮밥",count=0, price=3700, imagePath = @"/Static/치킨마요덮밥.jpg" },
+            new Food(){ category = Category.juice, name = "콜라", price=1500, count=0,imagePath = @"/Static/콜라.jpg" },
+            new Food(){ category = Category.juice, name = "사이다",price=1500, count=0,imagePath = @"/Static/칠성사이다.jpg" },
         };
-
         public UserControlOrder()
         {
             InitializeComponent();
-
             this.FontFamily = new System.Windows.Media.FontFamily("배달의민족 도현");
             this.Loaded += MainWindow_Loaded;
             this.listView.ItemsSource = calcaulation;
-            
+
+            tbTotalPrice.Text = totalPrice + "";
         }
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            lbCategory.SelectedIndex = 0; 
+            lbCategory.SelectedIndex = 0;
         }
 
         private void lbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (lbCategory.SelectedIndex == -1) return;
-
             Category category = (Category)lbCategory.SelectedIndex;
             lbMenus.ItemsSource = lstFood.Where(x => x.category == category).ToList();
         }
-
         private void lbMenus_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Food food = (Food)lbMenus.SelectedItem;
-            if (food != null)
+            int flag = 0;
+            //if (food != null)
+            //{
+                
+            //    calcaulation.Add(food);
+            //    //food.count++;
+            //    //int selectMenuPrice = 0;
+            //    for (int i = 0; i < calcaulation.Count; i++)
+            //    {
+            //        if(food.name == calcaulation[i].name)
+            //        {
+            //            calcaulation[i].count++;
+            //            break;
+            //        }
+            //        //selectMenuPrice = calcaulation[i].price;//지금 가져온 메뉴
+            //    }
+            //    calculationPrice();
+
+            //    //int intTbTotalPrice = int.Parse(tbTotalPrice.Text);//원래 있던 메뉴
+            //    listView.Items.Refresh();
+            //}
+
+            for(int i = 0; i< calcaulation.Count; i++)
+            {
+                if(food.name == calcaulation[i].name)
+                {
+                    calcaulation[i].count++;
+                    flag = 1;
+                    break;
+                }
+            }
+
+            
+            if (flag == 0)
             {
                 calcaulation.Add(food);
-                listView.Items.Refresh();
+                for (int i = 0; i < calcaulation.Count; i++)
+                {
+                    if (food.name == calcaulation[i].name)
+                    {
+                        calcaulation[i].count++;
+                    }
+                }
             }
+            calculationPrice();
+            listView.Items.Refresh();
         }
 
+        private void plusMinusThisMenu(object sender, RoutedEventArgs e)
+        {
+            var name = (sender as System.Windows.Controls.Button).Name;
+            var food = ((System.Windows.Controls.ListViewItem)listView.ContainerFromElement(sender as System.Windows.Controls.Button)).Content as Food;
+            if (name == "plus")
+            {
+                for (int i = 0; i < calcaulation.Count; i++)
+                {
+                    if (food.name == calcaulation[i].name)
+                    {
+                        calcaulation[i].count++;
+                        calculationPrice();
+
+                    }
+                }
+            }
+            else
+            {
+                for(int i = 0; i < calcaulation.Count; i++)
+                {
+                    if (food.name == calcaulation[i].name)
+                    {
+                        if (food.count == 1)
+                        {
+                            calcaulation[i].count = 0;
+                            Remove(food);
+                            calculationPrice();
+
+                        }
+                        else
+                        {
+                            calcaulation[i].count--;
+                            calculationPrice();
+
+                        }
+                    }
+                }
+            }
+            listView.Items.Refresh();
+
+        }
         private void btnMoveToHome(object sender, RoutedEventArgs e)
         {
             App.uIStateManager.SwitchCustomControl(CustomControlType.HOME);
         }
-
         private void btnMoveToPlace(object sender, RoutedEventArgs e)
         {
             App.uIStateManager.SwitchCustomControl(CustomControlType.PLACE);
         }
+        public void Remove(Food food)
+        {
+            
+        }
+        private void calculationPrice()
+        {
+            for (int i = 0; i < calcaulation.Count; i++)
+            {
+                if (calcaulation.Count - 1 == i)
+                {
+                    totalPrice += calcaulation[i].price * calcaulation[i].count;
+                }
+            }
 
-       
+
+            tbTotalPrice.Text = totalPrice + "";
+
+            //foreach ( Food food in calcaulation) {
+            //    totalPrice += food[i].price * food[i].count;
+            //}
+        }
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            calcaulation.Clear();
-            listView.Items.Refresh();
+            
+            if(System.Windows.MessageBox.Show("선택한 메뉴를 모두 삭제하시겠습니까?", "안내", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                calcaulation.Clear();
+                totalPrice = 0;
+                
+                calculationPrice();
+                listView.Items.Refresh();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("취소되었습니다, 안내");
+            }
         }
+
     }
 }
