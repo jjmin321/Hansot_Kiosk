b460fa9effd24c867265dd7b7edafb377e168e6b
jjmin321 <jjmin321@naver.com>
2020-11-02 15:53:12 +0900

    [ADD] Database/Dao & Database/Repository

---
+++ Hansot_Kiosk/Database/Repository/MenuRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hansot_Kiosk.Database.Repository
+{
+    static class MenuRepository
+    {
+        public static string FindAll = "SELECT idx, category, name, price, image FROM menu;";
+    }
+}
