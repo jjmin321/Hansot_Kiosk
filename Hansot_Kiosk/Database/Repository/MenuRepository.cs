d9d4c241699b5a2f83c349401f66b585389251e0
jjmin321 <jjmin321@naver.com>
2020-11-03 14:56:24 +0900

    [MODIFY] 데이터베이스 폴더 구조 변경

--- Hansot_Kiosk/Database/Repository/MenuRepository.cs
+++ Hansot_Kiosk/Database/Repository/MenuRepository.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using Hansot_Kiosk.Model;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Hansot_Kiosk.Database.Repository;
 
 namespace Hansot_Kiosk.Database.Repository
 {
-    static class MenuRepository
+    public class MenuRepository
     {
-        public static string FindAll = "SELECT idx, category, name, price, image FROM menu;";
+        private string FindAllMenu = "SELECT idx, category, name, price, image FROM menu;";
+        public List<Menu> GetMenus()
+        {
+            MySqlCommand cmd = new MySqlCommand(FindAllMenu, Connection.connection);
+            //ExecuteNonQuery() : insert, update, delete 사용시
+            //ExecuteReader() :select 사용시
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<Menu> menus = new List<Menu>();
+            while (reader.Read())
+            {
+                Menu menu = new Menu()
+                {
+                    Idx = Convert.ToInt32(reader["idx"]),
+                    Category = Convert.ToString(reader["category"]),
+                    Name = Convert.ToString(reader["name"]),
+                    Price = Convert.ToInt32(reader["price"]),
+                    Image = Convert.ToString(reader["image"]),
+                };
+
+                menus.Add(menu);
+            }
+            return menus;
+        }
     }
 }
