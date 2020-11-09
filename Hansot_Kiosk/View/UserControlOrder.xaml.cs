74d8757a2b4894785724a9b57e275762c41e6142
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-11-09 14:35:42 +0900

    [MODFIY] 메뉴 추가 & 총 가격 FIX

--- Hansot_Kiosk/View/UserControlOrder.xaml.cs
+++ Hansot_Kiosk/View/UserControlOrder.xaml.cs
@@ -24,7 +24,11 @@ namespace Hansot_Kiosk.View
     public partial class UserControlOrder : CustomControlModel, INotifyPropertyChanged
     {
         int index;
-        int startNumber = 0;
+        int lunchBoxstartNumber = 0;
+        int riceBowlstartNumber = 0;
+        int juiceStartNumber= 0;
+
+
         private List<Food> calcaulation = new List<Food>()
         {
             //담겨지는 리스트
@@ -206,11 +210,23 @@ namespace Hansot_Kiosk.View
             {
                 category = Category.lunchBox;
             }
-            for (int i = startNumber; i < lstFood.Count; i++)//전체 메뉴만큼 돌아서 그 안에서 카테고리가 선택된것에 리스트를 추가해주는것
+            for (int i = 0; i < lstFood.Count; i++)//전체 메뉴만큼 돌아서 그 안에서 카테고리가 선택된것에 리스트를 추가해주는것
             {
                 if (lstFood[i].category == category)//카테고리 확인
                 {
                     seeSixList.Add(lstFood[i]);
+                    if(category == Category.lunchBox)
+                    {
+                        lunchBoxstartNumber = 1;
+                    }
+                    else if (category == Category.RiceBowl)
+                    {
+                        riceBowlstartNumber = 8;
+                    }
+                    else
+                    {
+
+                    }
                     //카운터를 카테고리 메뉴 갯수를 세고 그 수에 /6 한다음에 남은것을 구함 -> 구한 값 만큼 for문을 돌려서
                 }
             }
@@ -224,11 +240,9 @@ namespace Hansot_Kiosk.View
 
             if (name == "next")
             {
-                startNumber += 5;
             }
             else
             {
-                startNumber -= 5;
             }
             dvideMenu();
             lbMenus.Items.Refresh();
