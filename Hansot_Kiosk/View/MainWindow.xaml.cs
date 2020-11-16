1385e678462036dae856a439657b7f15a8787635
NamTaeHee1 <skaxogml777@naver.com>
2020-11-16 16:03:52 +0900

    [MODIFY] mainWindow 되돌리기

--- Hansot_Kiosk/View/MainWindow.xaml.cs
+++ Hansot_Kiosk/View/MainWindow.xaml.cs
@@ -77,11 +77,6 @@ namespace Hansot_Kiosk.View
         private void btnMoveToHome(object sender, RoutedEventArgs e)
         {
             App.uIStateManager.SwitchCustomControl(CustomControlType.HOME);
-            if (UserControlSelectTable.CurButton != null) // 홈버튼 누를 경우 테이블 선택이 취소도니다.
-            {
-                UserControlSelectTable.CurButton.Background = new SolidColorBrush(ucSelectTable.basicColor);
-                UserControlSelectTable.CurButton = null;
-            }
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
